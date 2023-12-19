c646d0ea6e0ea1930b3b6d5a9efa72d799b5cb92
Haider <m.haiderashfaq@gmail.com>
2023-12-20 02:23:28 +0500

    setting screen modified

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Alert, StyleSheet, TextInput } from 'react-native';
+import { View, Text, TouchableOpacity, Alert, StyleSheet, TextInput,ScrollView,
+  RefreshControl, } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { auth, db } from '../firebase-config'
-import { updatePassword, reauthenticateWithCredential, EmailAuthProvider} from 'firebase/auth';
+import { updatePassword, reauthenticateWithCredential, EmailAuthProvider } from 'firebase/auth';
 import '@react-native-firebase/app';
 import LinearGradient from 'react-native-linear-gradient';
 import { doc, getDoc } from 'firebase/firestore';
@@ -26,13 +27,30 @@ const SettingsScreen: React.FC<SettingsScreenProps> = ({ navigation, route }) =>
   const [showPasswordInput, setShowPasswordInput] = useState(false);
   const [currentPassword, setCurrentPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
+  const [currentpasswordErrorMessage, setcurrentPasswordErrorMessage] = useState('');
+  const [newpasswordErrorMessage, setnewPasswordErrorMessage] = useState('');
+  const [refreshing, setRefreshing] = useState(false);
   const [userData, setUserData] = useState<UserData | null>(null);
-  
+
+  const handleRefresh = async () => {
+    setRefreshing(true);
+
+    // Reset state values to their initial state
+    setShowPasswordInput(false);
+    setCurrentPassword(""); // Clear the input after saving
+    setNewPassword(""); // Clear the input after saving
+    setcurrentPasswordErrorMessage("");
+    setnewPasswordErrorMessage("");
+
+    // Fetch any necessary data or perform other refresh logic here
+
+    setRefreshing(false);
+  };
 
   useEffect(() => {
     retrieveUserInfo();
     checkSession();
-    
+
   }, []);
 
   const retrieveUserInfo = async () => {
@@ -76,72 +94,97 @@ const SettingsScreen: React.FC<SettingsScreenProps> = ({ navigation, route }) =>
     }
   };
 
-//   const checkAuthentication = () => {
-//     const user = auth.currentUser;
+  //   const checkAuthentication = () => {
+  //     const user = auth.currentUser;
 
-//     if (user) {
-//         console.log('User is authenticated:', user.uid);
-//     } else {
-//         console.log('User is not authenticated');
-//     }
-// };
+  //     if (user) {
+  //         console.log('User is authenticated:', user.uid);
+  //     } else {
+  //         console.log('User is not authenticated');
+  //     }
+  // };
 
-// // Call the function to check authentication status
-// checkAuthentication();
+  // // Call the function to check authentication status
+  // checkAuthentication();
 
 
-const reauthenticate = async (currentPassword: string) => {
-  const user = auth.currentUser;
-
-  if (!user) {
-    console.log("User is not authenticated");
-    throw new Error("User is not authenticated");
-  }
-
-  const email = user.email;
-
-  if (!email) {
-    console.log("User's email is null");
-    throw new Error("User's email is null");
-  }
-
-  const cred = EmailAuthProvider.credential(email, currentPassword);
-  try {
-    await reauthenticateWithCredential(user, cred);
-    console.log("Reauthentication successful");
-    return true;
-  } catch (error:any) {
-    console.log("Reauthentication failed:", error.message);
-    throw new Error("Reauthentication failed");
-  }
-};
-
-const OnChangePassword = async () => {
-  if (!currentPassword || !newPassword) {
-    Alert.alert("Please enter both current and new passwords.");
-    return;
-  }
-
-  try {
-    await reauthenticate(currentPassword);
-
+  const reauthenticate = async (currentPassword: string) => {
     const user = auth.currentUser;
 
-    if (user) {
-      await updatePassword(user, newPassword);
-      Alert.alert("Password changed successfully. Please sign in again.");
-      navigation.replace('Sign in');
-    } else {
+    if (!user) {
+      console.log("User is not authenticated");
       throw new Error("User is not authenticated");
     }
-  } catch (error:any) {
-    Alert.alert("Failed to change password. " + "Please Enter the Correct Current Password.");
-  }
 
-  setShowPasswordInput(false);
-  setCurrentPassword(""); // Clear the input after saving
-  setNewPassword(""); // Clear the input after saving
-};
+    const email = user.email;
+
+    if (!email) {
+      console.log("User's email is null");
+      throw new Error("User's email is null");
+    }
+
+    const cred = EmailAuthProvider.credential(email, currentPassword);
+    try {
+      await reauthenticateWithCredential(user, cred);
+      console.log("Reauthentication successful");
+      return true;
+    } catch (error: any) {
+      console.log("Reauthentication failed:", error.message);
+      throw new Error("Reauthentication failed");
+    }
+  };
+
+  const OnChangePassword = async () => {
+
+    setcurrentPasswordErrorMessage('');
+    setnewPasswordErrorMessage('');
+
+    if (
+      currentPassword.trim() === '' ||
+      newPassword.trim() === ''
+
+    ) {
+
+      if (currentPassword.trim() === '') {
+        setcurrentPasswordErrorMessage('Current Password is required');
+      }
+
+      if (newPassword.trim() === '') {
+        setnewPasswordErrorMessage('New Password is required');
+      }
+
+      return;
+    }
+
+    if (!currentPassword || !newPassword) {
+      Alert.alert("Please enter both current and new passwords.");
+      return;
+    }
+    
+    try {
+      await reauthenticate(currentPassword);
+
+      const user = auth.currentUser;
+
+      if (user) {
+        await updatePassword(user, newPassword);
+        Alert.alert("Password changed successfully. Please sign in again.");
+        navigation.replace('Sign in');
+      } else {
+        throw new Error("User is not authenticated");
+      }
+    } catch (error: any) {
+
+      console.log('Error signing in:', error);
+      Alert.alert("Failed to change password. " + "Please Enter the Correct Current Password.");
+    }
+
+    setShowPasswordInput(false);
+    setCurrentPassword(""); // Clear the input after saving
+    setNewPassword(""); // Clear the input after saving
+
+
+  };
 
   const handleSignOut = async () => {
     await AsyncStorage.removeItem('session')
@@ -156,7 +199,13 @@ const OnChangePassword = async () => {
   };
 
   return (
-<LinearGradient colors={['black', 'purple']} style={styles.container}>
+
+    <ScrollView
+     refreshControl={
+       <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} colors={['#0000ff']} />
+     }
+   >
+    <LinearGradient colors={['black', 'purple']} style={styles.container}>
 
       <View>
         <Text style={styles.headerText}>My Account</Text>
@@ -175,13 +224,15 @@ const OnChangePassword = async () => {
 
         {showPasswordInput && (
           <>
-           <TextInput
+            <TextInput
               style={styles.input}
               placeholder="Type current password"
               secureTextEntry
               value={currentPassword}
               onChangeText={setCurrentPassword}
             />
+
+            {currentpasswordErrorMessage ? <Text style={{ color: 'red', marginBottom: 10, marginLeft:15 }}>{currentpasswordErrorMessage}</Text> : null}
 
             <TextInput
               style={styles.input}
@@ -191,17 +242,20 @@ const OnChangePassword = async () => {
               onChangeText={setNewPassword}
             />
 
+            {newpasswordErrorMessage ? <Text style={{ color: 'red', marginBottom: 10, marginLeft:15 }}>{newpasswordErrorMessage}</Text> : null}
+
             <TouchableOpacity onPress={OnChangePassword} style={[styles.saveButton, { backgroundColor: 'yellow' }]}>
               <Text style={[styles.buttonText, { color: 'black' }]}>Save</Text>
             </TouchableOpacity>
           </>
         )}
 
-        <TouchableOpacity style={{marginTop:350,backgroundColor: 'red',borderRadius: 30,paddingVertical: 12,paddingHorizontal: 24,alignItems: 'center',}} onPress={handleSignOut}>
-          <Text style={{ fontSize: 16, fontWeight: 'bold',textAlign: 'center',color:"black"}}>Log out</Text>
+        <TouchableOpacity style={{ marginTop: 350, backgroundColor: 'red', borderRadius: 30, paddingVertical: 12, paddingHorizontal: 24, alignItems: 'center', }} onPress={handleSignOut}>
+          <Text style={{ fontSize: 16, fontWeight: 'bold', textAlign: 'center', color: "black" }}>Log out</Text>
         </TouchableOpacity>
       </View>
     </LinearGradient>
+     </ScrollView>
   );
 };
 
@@ -209,14 +263,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#111',
-    padding: 20,
+    padding: 80,
+    paddingHorizontal: 20,
+    paddingTop: 50,
   },
   headerText: {
     fontSize: 30,
     color: 'white',
     textAlign: 'center',
     fontWeight: 'bold',
-    marginTop: 5,
+    marginBottom: 30,
   },
   infoContainer: {
     borderTopWidth: 4,
@@ -228,7 +284,7 @@ const styles = StyleSheet.create({
   },
   infoText: {
     color: 'white',
-    marginTop: 15,
+    marginTop: 9,
     fontSize: 22,
     fontWeight: 'bold',
   },
